8283d69459857c59a96de05c643742d05a81da9f
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-06-19 19:53:17 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # Write your code below game_hash
 def game_hash
   {
@@ -125,5 +126,92 @@ def game_hash
     }
   }
 end
+#binding.pry
+def num_points_scored(player_name)
+  game_hash.each do |key, value|
+    value[:players].each do |play_ele|
+      if play_ele[:player_name] == player_name
+      return play_ele[:points]
+    #binding.pry
+      end
+    end
+  end
+  #takes players name and returns the points scored for that player
+  # Write code here
+end
 
-# Write code here
+def shoe_size(player_name)
+  game_hash.each do |key, value|
+    value[:players].each do |play_ele|
+      if play_ele[:player_name] == player_name
+      return play_ele[:shoe]
+      end
+    end
+  end
+  #takes players name and returns the shoe size for that player
+end
+
+def team_colors(team_name)
+  game_hash.each do |key, value|
+    if value[:team_name] == team_name
+      return value[:colors]
+    end
+    #binding.pry
+  end
+  #takes team name and returns an array of the teams colors
+end
+
+def team_names
+  results_ary = []
+    game_hash.each do |key, value|
+      if game_hash[key][:team_name]
+       results_ary << game_hash[key][:team_name]
+       end
+     end
+     #binding.pry
+     results_ary
+end  #uses game_hash to return an array of the team namea
+
+def player_numbers(team_name)
+  jersey_num = []
+  game_hash.each do |key, value|
+    value[:players].each do |player|
+      if game_hash[key][:team_name] == team_name
+        jersey_num << player[:number]
+      end
+    end
+  end
+#  binding.pry
+  jersey_num
+end
+
+
+
+def player_stats(player_name)
+  game_hash.each do |key, value|
+    value[:players].each do |play_ele| #second block parameter has to be named something else
+      if play_ele[:player_name] == player_name
+        return play_ele
+  #  binding.pry
+    #if player_name == value
+      #return game_hash[:home][:players][0]
+        end
+      end
+    end
+  #returns a hash of that players stats
+end
+
+def big_shoe_rebounds
+  rebounds = 0
+  shoe_size = 0
+  game_hash.each do |key, value|
+    value[:players].each do |player|
+      if player[:shoe] > shoe_size
+        shoe_size = player[:shoe]
+        rebounds = player[:rebounds]
+      end
+    end
+  end
+  #binding.pry
+  rebounds
+end
